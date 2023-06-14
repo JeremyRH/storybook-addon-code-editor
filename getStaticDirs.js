baca6c48c17c939b149f296d1227a56e5715a394
JeremyRH <holcomb227@gmail.com>
2023-06-13 21:18:05 -0700

    fix: resolve package location without require.resolve

--- getStaticDirs.js
+++ getStaticDirs.js
@@ -1,20 +1,31 @@
+const fs = require('fs');
+const { createRequire } = require('module');
 const path = require('path');
 
-function getStaticDir(specifier) {
+function getStaticDir(specifier, relativeToFile = __filename) {
   const [pn1, pn2, ...pathParts] = specifier.split('/');
   const isScopedPackage = !!(specifier.startsWith('@') && pn2);
   const packageName = isScopedPackage ? `${pn1}/${pn2}` : pn1;
   const dirPath = (isScopedPackage ? pathParts : [pn2, ...pathParts]).join('/');
-  const packageDir = path.dirname(require.resolve(`${packageName}/package.json`));
+  const require = createRequire(relativeToFile);
+  const packageDir = require.resolve
+    .paths(specifier)
+    .map((p) => path.join(p, packageName))
+    .find((p) => fs.existsSync(p));
+
+  if (!packageDir) {
+    throw new Error(`Cannot find module '${packageName}'`);
+  }
+
   return {
     from: path.join(packageDir, dirPath),
     to: path.posix.join(packageName, dirPath),
   };
 }
 
-function tryGetStaticDir(packageName, path) {
+function tryGetStaticDir(packageName, relativeToFile) {
   try {
-    return getStaticDir(packageName, path);
+    return getStaticDir(packageName, relativeToFile);
   } catch (err) {}
 }
 
