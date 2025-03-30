d3f8d3fff6815a34373ebcb35c74ebb42febdd06
JeremyRH <holcomb227@gmail.com>
2025-03-30 15:50:44 -0700

    chore: Update monaco.

--- src/Editor/monacoLoader.ts
+++ src/Editor/monacoLoader.ts
@@ -1,7 +1,7 @@
 import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 function injectScript(url: string) {
-  return new Promise((resolve, reject) => {
+  return new Promise<Event>((resolve, reject) => {
     const script = document.createElement('script');
     script.src = url;
     script.defer = true;
@@ -12,11 +12,12 @@ function injectScript(url: string) {
 }
 
 export function monacoLoader(): Promise<typeof Monaco> {
-  return injectScript('monaco-editor/min/vs/loader.js').then(() => {
+  const relativeLoaderScriptPath = 'monaco-editor/min/vs/loader.js';
+  return injectScript(relativeLoaderScriptPath).then((e) => {
+    const loaderScriptSrc: string = (e.target as any)?.src || window.location.origin + '/';
+    const baseUrl = loaderScriptSrc.replace(relativeLoaderScriptPath, '');
     return new Promise((resolve) => {
-      (window as any).require.config({
-        paths: { vs: 'monaco-editor/min/vs' },
-      });
+      (window as any).require.config({ paths: { vs: `${baseUrl}monaco-editor/min/vs` } });
       (window as any).require(['vs/editor/editor.main'], resolve);
     });
   });
